b2a244b0ecdc657c92e15d3fdf7789ebceca280a
Jase HA <mindwing@gmail.com>
2015-07-23 00:38:15 +0900

    Update PolymorphismTest1.java

--- PolymorphismTest1.java
+++ PolymorphismTest1.java
@@ -15,10 +15,6 @@ class DefaultType {
 }
 
 public class PolymorphismTest1 {
-	public void printObject(Object obj) {
-		System.out.println(obj.toString());
-	}
-
 	public static void main(String[] args) {
 		System.out.println(new MyType("my type"));
 		System.out.println(new DefaultType());
